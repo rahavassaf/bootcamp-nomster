8a1a5438411e92ceeec6201d65499f9c53180223
Assaf Rahav <rahav.assaf@gmail.com>
2019-05-31 04:43:53 +0000

    perform address validation before submit or edit

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,18 @@
+class PlaceValidator < ActiveModel::Validator
+  def validate(record)
+    if record.latitude.blank? || record.longitude.blank?
+      record.errors.add(:address, 'could not be found')
+    end
+  end
+end
+
 class Place < ApplicationRecord
 	validates :name, presence: true, length: { maximum: 50, minimum: 3 }
 	validates :address, presence: true, length: { maximum: 75,  minimum: 10 }
 	validates :description, presence: true, length: { maximum: 500,  minimum: 10 }
-
+	validates_with PlaceValidator
 	belongs_to :user
+	
 	geocoded_by :address
-  	after_validation :geocode
+	before_validation :geocode, if: :address_changed?
 end
