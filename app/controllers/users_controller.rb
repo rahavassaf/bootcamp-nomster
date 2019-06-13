9ea9c64e305a56a8b9dd078cd2ba846f81b17193
Assaf Rahav <rahav.assaf@gmail.com>
2019-06-13 04:20:23 +0000

    clean up query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
 	def my_favorites
 		# this one was interesting lol
-		@places = Place.where(id: User.find(current_user.id).likes.pluck(:place_id)).paginate(page: params[:page], per_page: 10)
+		@places = Place.where(id: current_user.likes.pluck(:place_id)).paginate(page: params[:page], per_page: 10)
 	end
 
 	def setavatar
